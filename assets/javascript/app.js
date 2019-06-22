ba78c4a44d2f63b7fa01651e1727087c83542824
Rizwan <rizwanrensa@gmail.com>
2019-06-22 19:20:20 -0400

    rendering first question on the scree

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,61 @@
-var questions = {
-  'Where would you go if you wanted to see a whale?': [
-    'ALberta',
-    'British Columbia',
-    'Saskatchewan',0
-  ]
-};
+//Get all the elements from the DOM - as we need to update the inner HTML
+var start = $('#start')
+var quiz = $('#quiz');
+var question = $('#question')
+var counter = $('#counter')
+var timeGuage = $('#timeGauge')
+var choiceA = $('#A')
+var choiceB = $('#B')
+var choiceC = $('#C')
+var progress = $('#progress')
+var score = $('#scorecontainer')
+
+//Create questions, choices and correct inside an array 
+const questions = [{
+    question: "Where would you go if you wanted to see a whale? ",
+    choiceA: "Alberta",
+    choiceB: "British Columbia",
+    choiceC: "Saskatchewan",
+    correct: "B"
+
+  },
+  {
+    question: "Where would you go if you wanted to see a whale? ",
+    choiceA: "Alberta",
+    choiceB: "British Columbia",
+    choiceC: "Saskatchewan",
+    correct: "B"
+
+  },
+  {
+    question: "If you wanted to see a loon, where would you go?",
+    choiceA: "a farm",
+    choiceB: "a lake",
+    choiceC: "a forest",
+    correct: "B"
+
+  },
+]
+
+//#Render question on the screen
+
+//1. last question is the length of the question-1 in the array 
+let lastQuestion = questions.length - 1;
+//2. current question that the user is viewing; setting to zero
+let currectQuestionIndex = 0;
 
 
-$('#form-trivia input').on('change', function() {
-  alert($('input[name=radioName]:checked', '#form-trivia').val());
-});
 
-//$('input:radio[name=sex]:checked').val();
+//3. 
+function renderQuestion() {
+  //select the  question at index 0 -display first question
+  let q = questions[currectQuestionIndex];
+  $('#question').text = q.question
+  question.html("<p>" + q.question + "</p>")
+  choiceA.text(q.choiceA);
+  choiceB.text(q.choiceB);
+  choiceC.text(q.choiceC)
+
+}
+
+renderQuestion()
